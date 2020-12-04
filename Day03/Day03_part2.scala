dd8976bd5ee2e66a062efbc871eeae888ffa5f99
Andrew Webb <andrew@awebb.info>
2020-12-04 23:42:18 +0000

    Shifted the final keyword from the traverse method to the TreeMap class - don't want to ever extend the class

--- Day03/Day03_part2.scala
+++ Day03/Day03_part2.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 import scala.annotation.tailrec
 
-class TreeMap(input_lines: Iterator[String]) {
+final class TreeMap(input_lines: Iterator[String]) {
   private[this] def parse_line(line: String) = line.map(_ == '#')
   private[this] val internal_map = input_lines.toVector.map(parse_line)
 
@@ -11,7 +11,7 @@ class TreeMap(input_lines: Iterator[String]) {
   private[this] def treemap(y: Int)(x: Int) = internal_map(y)(x % width)
 
   @tailrec
-  final def traverse_from(y: Int, x: Int, dy: Int, dx: Int, trees_hit: Int = 0): Int =
+  def traverse_from(y: Int, x: Int, dy: Int, dx: Int, trees_hit: Int = 0): Int =
     if (y >= height) trees_hit else {
       val to_add = if (treemap(y)(x)) 1 else 0
       traverse_from(y + dy, x + dx, dy, dx, trees_hit + to_add)
