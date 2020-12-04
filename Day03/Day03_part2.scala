5ac9cfbb09567cded2ee7ba3795f070d605f0a44
Andrew Webb <andrew@awebb.info>
2020-12-04 23:38:10 +0000

    Made parse_line method private, added tailrec annotations, made recursive functions final so it can do tail call optimization

--- Day03/Day03_part2.scala
+++ Day03/Day03_part2.scala
@@ -1,16 +1,17 @@
 import scala.io.Source
+import scala.annotation.tailrec
 
 class TreeMap(input_lines: Iterator[String]) {
-  def parse_line(line: String) =
-    line.map(_ == '#')
-
+  private[this] def parse_line(line: String) = line.map(_ == '#')
   private[this] val internal_map = input_lines.toVector.map(parse_line)
+
   private[this] val height = internal_map.size
   private[this] val width = internal_map(0).size
 
   private[this] def treemap(y: Int)(x: Int) = internal_map(y)(x % width)
 
-  def traverse_from(y: Int, x: Int, dy: Int, dx: Int, trees_hit: Int = 0): Int =
+  @tailrec
+  final def traverse_from(y: Int, x: Int, dy: Int, dx: Int, trees_hit: Int = 0): Int =
     if (y >= height) trees_hit else {
       val to_add = if (treemap(y)(x)) 1 else 0
       traverse_from(y + dy, x + dx, dy, dx, trees_hit + to_add)
