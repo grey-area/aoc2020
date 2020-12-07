2d421aade8d36d3f50edad84ed11ac36c94716d0
Andrew Webb <andrew@awebb.info>
2020-12-07 20:24:55 +0000

    Day 7

---
+++ Day07/Day07_1.scala
@@ -0,0 +1,40 @@
+import io.Source
+
+object Graph {
+  def fromBagPattern = raw"^(\w+\s\w+)\sbag".r
+  def toBagPattern = raw"(?<=\s)(\d+)\s(\w+\s\w+)\sbag".r
+
+  def parseLine(line: String) = {
+    val fromColour = fromBagPattern.findFirstMatchIn(line).map(_.group(1))
+    val toColours = toBagPattern.findAllMatchIn(line).map{_.group(2)}.toSet
+
+    fromColour match {
+      case Some(v) => Some(v -> toColours)
+      case None => None
+    }
+  }
+
+  def apply(lines: Iterator[String]) = {
+    val graphMap = Map(lines.map(parseLine).toList.flatten: _*)
+    new Graph(graphMap)
+  }
+}
+
+final class Graph(private val graphMap: Map[String, Set[String]]) {
+  // Note: inefficient, repeated computation
+  def canReach(key: String, target: String): Boolean = {
+    val values = graphMap(key)
+    values.size > 0 && (values.contains(target) ||
+      values.map(canReach(_, target)).reduce(_ || _))
+  }
+
+  def numberReaching(target: String) =
+    graphMap.keys.toList.map(x => if (canReach(x, target)) 1 else 0).sum
+}
+
+object Day07 extends App {
+  val lines = Source.fromFile("inputs/input.txt").getLines()
+  val graph = Graph(lines)
+
+  println(graph.numberReaching("shiny gold"))
+}
---
+++ Day07/Day07_2.scala
@@ -0,0 +1,43 @@
+import io.Source
+
+final case class Edge(toName: String, number: Int)
+
+object Graph {
+  def fromBagPattern = raw"^(\w+\s\w+)\sbag".r
+  def toBagPattern = raw"(?<=\s)(\d+)\s(\w+\s\w+)\sbag".r
+
+  def parseLine(line: String) = {
+    val fromColour = fromBagPattern.findFirstMatchIn(line).map(_.group(1))
+    val toColours = toBagPattern.findAllMatchIn(line).map{
+      m => Edge(m.group(2), m.group(1).toInt)
+    }.toList
+
+    fromColour match {
+      case Some(v) => Some(v -> toColours)
+      case None => None
+    }
+  }
+
+  def apply(lines: Iterator[String]) = {
+    val graphMap = Map(lines.map(parseLine).toList.flatten: _*)
+    new Graph(graphMap)
+  }
+}
+
+final class Graph(private val graphMap: Map[String, List[Edge]]) {
+  // Note: inefficient, repeated computation
+  def countSubBags(key: String): Int = {
+    val values = graphMap(key)
+    if (values.size == 0)
+      0
+    else
+      values.map(edge => edge.number * (1 + countSubBags(edge.toName))).sum
+  }
+}
+
+object Day07 extends App {
+  val lines = Source.fromFile("inputs/input.txt").getLines()
+  val graph = Graph(lines)
+
+  println(graph.countSubBags("shiny gold"))
+}
