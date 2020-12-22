73d459597b12e808ccd830b89042a54e33ed4f1f
Andrew Webb <andrew@awebb.info>
2020-12-22 11:57:40 +0000

    Day 22 part 2 refactor

--- Day22/Day22_2.scala
+++ Day22/Day22_2.scala
@@ -13,29 +13,32 @@ object Game {
 }
 
 final case class Game(hands: List[Vector[Int]]) {
-  def play(history: Set[Game] = Set.empty): (Int, Vector[Int]) =
+  private def makeNewHands(roundWinnerID: Int) = {
+    val heads = List(hands(roundWinnerID).head, hands(1 - roundWinnerID).head)
+    val tails = hands.map(_.tail)
+    tails.updated(roundWinnerID, tails(roundWinnerID) ++ heads)
+  }
+
+  private def computeRoundWinner(hd1: Int, hd2: Int, tl1: Vector[Int], tl2: Vector[Int]) = {
+    if (tl1.size >= hd1 && tl2.size >= hd2)
+      Game(List(tl1.take(hd1), tl2.take(hd2))).playRecursive()._1
+    else if (hd1 > hd2) 0 else 1
+  }
+
+  private def playRecursive(history: Set[Game] = Set.empty): (Int, Vector[Int]) =
     if (history.contains(this) || hands(1).isEmpty)
       (0, hands(0))
     else if (hands(0).isEmpty)
       (1, hands(1))
     else hands match {
       case (hd1 +: tl1) :: (hd2 +: tl2) :: Nil => {
-        val roundWinnerID =
-          if (tl1.size >= hd1 && tl2.size >= hd2) {
-            val (winnerID, _) = Game(List(tl1.take(hd1), tl2.take(hd2))).play()
-            winnerID
-          }
-          else
-            if (hd1 > hd2) 0 else 1
-        val heads = List(hands(roundWinnerID).head, hands(1 - roundWinnerID).head)
-        val newHands =
-          if (roundWinnerID == 0)
-            List(tl1 ++ heads, tl2)
-          else
-            List(tl1, tl2 ++ heads)
-        Game(newHands).play(history + this)
+        val roundWinnerID = computeRoundWinner(hd1, hd2, tl1, tl2)
+        val newHands = makeNewHands(roundWinnerID)
+        Game(newHands).playRecursive(history + this)
       }
     }
+
+  def play() = playRecursive()._2
 }
 
 
@@ -43,7 +46,7 @@ object Day22 extends App {
   val text = io.Source.fromFile("inputs/input.txt").mkString
 
   val game = Game.parseText(text)
-  val (_, winningHand) = game.play()
+  val winningHand = game.play()
   val answer = Game.computeScore(winningHand)
   println(answer)
 }
