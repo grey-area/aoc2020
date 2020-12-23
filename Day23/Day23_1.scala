7b6f8b79a99da295d1cbf19f6a529547506ac11a
Andrew Webb <andrew@awebb.info>
2020-12-23 10:51:24 +0000

    Day 23 part 1

---
+++ Day23/Day23_1.scala
@@ -0,0 +1,42 @@
+object Cups {
+  private def rotate(values: Vector[Int], n: Int) =
+    values.drop(n) ++ values.take(n)
+
+  def parseInput(fname: String) =
+    Cups(io.Source.fromFile(fname).mkString.map(_.toString.toInt).toVector)
+}
+
+final case class Cups(values: Vector[Int]) {
+  private def findNextValue(removed: Vector[Int], currentValue: Int) = {
+    val values = ((currentValue - 1 until 0 by -1) ++ (9 until currentValue by -1))
+    values.find(v => ! (removed contains v)).get
+  }
+
+  private def removeAndInsert() = {
+    val removed = values.slice(1, 4)
+    val remaining = values.head +: values.slice(4, values.size)
+    val destinationIndex = remaining.indexOf(findNextValue(removed, values(0))) + 1
+    remaining.take(destinationIndex) ++ removed ++ remaining.drop(destinationIndex)
+  }
+
+  def computeAnswer() = {
+    val idx = values.indexOf(1)
+    val rotated = Cups.rotate(values, idx)
+    rotated.drop(1).mkString
+  }
+
+  def play(n: Int): String =
+    if (n == 0)
+      computeAnswer()
+    else {
+      val updatedValues = Cups.rotate(removeAndInsert(), 1)
+      Cups(updatedValues).play(n - 1)
+    }
+}
+
+
+object Day23 extends App {
+  val cups = Cups.parseInput("inputs/input.txt")
+  val answer = cups.play(100)
+  println(answer)
+}
